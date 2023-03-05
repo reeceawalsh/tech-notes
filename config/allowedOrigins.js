0f840301db1d6826658c1eab6d89ddbe502556d5
reeceawalsh <reeceawalsh@hotmail.co.uk>
2023-03-05 13:29:31 +0000

    Setup cors config to enable fetching from allowed origins

---
+++ config/allowedOrigins.js
@@ -0,0 +1,9 @@
+// list of websites that can fetch this website
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:3500',
+  'https://www.reecewalsh.com',
+  'https://reecewalsh.com'
+]
+
+module.exports = allowedOrigins
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-
 const { logger } = require('./middleware/logger')
 const errorHandler = require('./middleware/errorHandler')
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 
 const PORT = process.env.PORT || 3500
 
 app.use(logger) 
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(cookieParser())
 
