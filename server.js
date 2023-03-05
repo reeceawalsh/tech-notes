36ecf19b1b6c833b91cccbe5093f1a66a49b1abc
reeceawalsh <reeceawalsh@hotmail.co.uk>
2023-03-05 13:00:21 +0000

    Installed date-fns and uuid

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 3500
 
-app.use('/', express.static(path.join(__dirname, '/public'))) // __dirname is a global variable (look in folder we are in). 
+app.use(express.json())
+
+app.use('/', express.static(path.join(__dirname, 'public'))) // __dirname is a global variable (look in folder we are in). 
 
 app.use('/', require('./routes/root')) // looking for routes folder and root file
 
+// deals with everything that didn't match anything above
+// this code should be at the end of the routes but before the app.listen
 app.all('*', (req, res) => {
   res.status(404)
   if (req.accepts('html')) {
@@ -17,7 +21,6 @@ app.all('*', (req, res) => {
     res.type('txt').send('404 Not Found')
   }
 })
-
 
 // will point to the server destination 
 app.listen(PORT, () => console.log(`Server available at http://localhost:${PORT}`))
